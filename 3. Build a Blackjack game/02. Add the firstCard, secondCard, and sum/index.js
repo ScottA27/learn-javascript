79881d26bdd2e61b5e8edbfc36e6a6cf970928e2
ScottA27 <scotta27501@gmail.com>
2024-01-19 16:40:47 +0000

    Blackjack

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,11 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.floor(Math.random() * 11 + 1);
+let secondCard = Math.floor(Math.random() * 11 + 1);
+
+let sum = firstCard + secondCard;
+
+console.log(sum);
--- 3. Build a Blackjack game/03. If...else conditionals/index.js
+++ 3. Build a Blackjack game/03. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
 
+if (sum < 21) {
+  console.log("Do you want to draw another card?");
+} else if (sum === 21) {
+  console.log("You got blackjack!");
+} else {
+  console.log("You've gone bust!");
+}
